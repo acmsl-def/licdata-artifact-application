2fe688231c46686f19cc7e28646f5151288a4c20
rydnr <github@acm-sl.org>
2025-01-17 20:05:33 +0100

    Fixed unpackPhase

--- flake.nix
+++ flake.nix
@@ -206,16 +206,15 @@
             # pythonImportsCheck = [ pythonpackage ];
 
             unpackPhase = ''
-              command cp -r ${src} .
-              sourceRoot=$(command ls | command grep -v env-vars)
-              command chmod -R +w $sourceRoot
-              command cp ${pyprojectToml} $sourceRoot/pyproject.toml
-              command cp ${bannerTemplate} $sourceRoot/${banner_file}
-              command cp ${entrypointTemplate} $sourceRoot/entrypoint.sh
+              command cp -r ${src}/* .
+              command chmod -R +w .
+              command cp ${pyprojectToml} ./pyproject.toml
+              command cp ${bannerTemplate} ./${banner_file}
+              command cp ${entrypointTemplate} ./entrypoint.sh
             '';
 
             postPatch = ''
-              substituteInPlace /build/$sourceRoot/entrypoint.sh \
+              substituteInPlace ./entrypoint.sh \
                 --replace "@SOURCE@" "$out/bin/${entrypoint}.sh" \
                 --replace "@PYTHONEDA_EXTRA_NAMESPACES@" "org" \
                 --replace "@PYTHONPATH@" "$PYTHONPATH" \
@@ -228,17 +227,15 @@
             '';
 
             postInstall = with python.pkgs; ''
-              command pushd /build/$sourceRoot
               for f in $(command find . -name '__init__.py' | sed 's ^\./  g'); do
                 if [[ ! -e $out/lib/python${pythonMajorMinorVersion}/site-packages/$f ]]; then
                   command mkdir -p $out/lib/python${pythonMajorMinorVersion}/site-packages/"$(command dirname $f)";
                   command cp -r "$(command dirname $f)"/* $out/lib/python${pythonMajorMinorVersion}/site-packages/"$(command dirname $f)";
                 fi
               done
-              command popd
               command mkdir -p $out/bin $out/dist $out/deps/flakes $out/deps/nixpkgs
               command cp dist/${wheelName} $out/dist
-              command cp /build/$sourceRoot/entrypoint.sh $out/bin/${entrypoint}.sh
+              command cp ./entrypoint.sh $out/bin/${entrypoint}.sh
               command chmod +x $out/bin/${entrypoint}.sh
               command echo '#!/usr/bin/env sh' > $out/bin/banner.sh
               command echo "export PYTHONPATH=$PYTHONPATH" >> $out/bin/banner.sh
